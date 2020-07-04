01fc2849269c3dd08cb81ac12c15326962d870d8
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2020-07-04 14:51:12 +0300

    layout update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*toggle menu*/
 function myFunction(x) {
   x.classList.toggle("change");
 }
@@ -13,3 +14,15 @@ menuToggler.addEventListener('click', function() {
 	body.classList.toggle('open');
 
 });
+
+window.onscroll = function() {myScroll()};
+function myScroll(){
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	if (winScroll > 100) {
+		document.getElementById("menu-opacity").style.opacity = 0;
+	} 
+	else {
+		document.getElementById("menu-opacity").style.opacity = 1;
+	}
+  	
+}
